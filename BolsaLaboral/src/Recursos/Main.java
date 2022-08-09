9586d57901e62fe55cc30cb01f6be25e149ce7e2
aneurystvz <aneurystvz@localhost>
2022-08-09 13:28:33 -0400

    commit 2

--- BolsaLaboral/src/Recursos/Main.java
+++ BolsaLaboral/src/Recursos/Main.java
@@ -7,45 +7,45 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
-		ArrayList<String> idiomas = new ArrayList<>();
-		idiomas.add("Español");
-		idiomas.add("Ingles");
+//		ArrayList<String> idiomas = new ArrayList<>();
+//		idiomas.add("Español");
+//		idiomas.add("Ingles");
+//		
+//		ArrayList<String> idiomas2 = new ArrayList<>();
+//		idiomas2.add("Español");
 		
-		ArrayList<String> idiomas2 = new ArrayList<>();
-		idiomas2.add("Español");
-		
-		Universitario u1 = new Universitario("031", "Jose", 25, "Masculino", "809", "Santiago", "Santiago", "Ing Sistemas");
-		Universitario u2 = new Universitario("402", "Juan", 35, "Masculino", "849", "Santo Domingo", "Santo Domingo", "Ing Sistemas");
-		Tecnico t3 = new Tecnico("444", "mario", 20, "Masculino", "444", "Santiago", "Santiago", "Electricista");
-		
-		SolicitudPersona s1 = new SolicitudPersona("S1", u1, 4, 18, "PartTime", "si", "No", idiomas2);
-		SolicitudPersona s2 = new SolicitudPersona("S2", u2, 3, 35, "PartTime", "si", "Si", idiomas);
-		SolicitudPersona s3 = new SolicitudPersona("S3", t3, 3, 15, "Completo", "si", "Si", idiomas);
-		
-		u1.getMisSolicitudes().add(s1);
-		u2.getMisSolicitudes().add(s2);
-		t3.getMisSolicitudes().add(s3);
-		
-		Empresa e = new Empresa("E1", "Homs", "salud", "santiago", "809", "Santiago");
-		SolicitudEmpresa se1 = new SolicitudEmpresa(e, 1, 3, 35, "Completo", "Si", "Universitario", "ing sistemas", 18, idiomas);
-		e.getSolicitudes().add(se1);
-		
-		Control.getInstance().getPersonas().add(u1);
-		Control.getInstance().getPersonas().add(u2);
-		Control.getInstance().getPersonas().add(t3);
-		Control.getInstance().getSoliPersonas().add(s1);
-		Control.getInstance().getSoliPersonas().add(s2);
-		Control.getInstance().getSoliPersonas().add(s3);
-		Control.getInstance().getEmpresas().add(e);
-		Control.getInstance().getSoliEmpresas().add(se1);
-		
-		//Persona p = Control.getInstance().match();
-		
-		//System.out.println(p.getNombre());
-		System.out.println(Control.getInstance().porcentaje(s1, se1));
-		System.out.println(Control.getInstance().porcentaje(s2, se1));
-		System.out.println(Control.getInstance().checkSimilitud(s3, se1));
-		System.out.println(Control.getInstance().porcentaje(s3, se1));
+//		Universitario u1 = new Universitario("031", "Jose", 25, "Masculino", "809", "Santiago", "Santiago", "Ing Sistemas");
+//		Universitario u2 = new Universitario("402", "Juan", 35, "Masculino", "849", "Santo Domingo", "Santo Domingo", "Ing Sistemas");
+//		Tecnico t3 = new Tecnico("444", "mario", 20, "Masculino", "444", "Santiago", "Santiago", "Electricista");
+//		
+//		SolicitudPersona s1 = new SolicitudPersona("S1", u1, 4, 18, "PartTime", "si", "No", idiomas2);
+//		SolicitudPersona s2 = new SolicitudPersona("S2", u2, 3, 35, "PartTime", "si", "Si", idiomas);
+//		SolicitudPersona s3 = new SolicitudPersona("S3", t3, 3, 15, "Completo", "si", "Si", idiomas);
+//		
+//		u1.getMisSolicitudes().add(s1);
+//		u2.getMisSolicitudes().add(s2);
+//		t3.getMisSolicitudes().add(s3);
+//		
+//		Empresa e = new Empresa("E1", "Homs", "salud", "santiago", "809", "Santiago");
+//		SolicitudEmpresa se1 = new SolicitudEmpresa(e, 1, 3, 35, "Completo", "Si", "Universitario", "ing sistemas", 18, idiomas);
+//		e.getSolicitudes().add(se1);
+//		
+//		Control.getInstance().getPersonas().add(u1);
+//		Control.getInstance().getPersonas().add(u2);
+//		Control.getInstance().getPersonas().add(t3);
+//		Control.getInstance().getSoliPersonas().add(s1);
+//		Control.getInstance().getSoliPersonas().add(s2);
+//		Control.getInstance().getSoliPersonas().add(s3);
+//		Control.getInstance().getEmpresas().add(e);
+//		Control.getInstance().getSoliEmpresas().add(se1);
+//		
+//		//Persona p = Control.getInstance().match();
+//		
+//		//System.out.println(p.getNombre());
+//		System.out.println(Control.getInstance().porcentaje(s1, se1));
+//		System.out.println(Control.getInstance().porcentaje(s2, se1));
+//		System.out.println(Control.getInstance().checkSimilitud(s3, se1));
+//		System.out.println(Control.getInstance().porcentaje(s3, se1));
 	
 	}
 }
